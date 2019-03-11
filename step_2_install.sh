69e49afdd400dec919178bb911b269b28657196a
zouxz <zouxz182@163.com>
2019-03-11 16:24:18 +0800

    Deleted dpkg-warning step_1_install step_2_install

--- step_2_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-for package in $(cat dpkg-warning.txt | grep "dpkg: warning: files list file for package " | grep -Po "'[^']*'" | sed "s/'//g");
-do
-	sudo apt -y install --reinstall   "$package";
-	#如果没有安装aptitude, 则可以用apt-get --reinstall "$package";
-done
-
